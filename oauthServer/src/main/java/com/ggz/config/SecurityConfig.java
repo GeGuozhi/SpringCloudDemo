a6b3cb90585fe42de2b07fbb007d7f910db5230d
GeGuozhi <1462496578@qq.com>
2021-03-15 00:35:36 +0800

    怎么合并~~

---
+++ oauthServer/src/main/java/com/ggz/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+//package com.ggz.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.builders.WebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests().antMatchers("/**").fullyAuthenticated().and().httpBasic();
+//    }
+//}
--- oauthServer/src/main/java/com/ggz/service/CustomUserDetailsService.java
+++ oauthServer/src/main/java/com/ggz/service/CustomUserDetailsService.java
@@ -1,36 +1,36 @@
-package com.ggz.service;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@Component(value = "customUserDetailsService")
-public class CustomUserDetailsService implements UserDetailsService {
-
-    private final PasswordEncoder passwordEncoder;
-
-    public CustomUserDetailsService(PasswordEncoder passwordEncoder) {
-        this.passwordEncoder = passwordEncoder;
-    }
-
-    @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        if(!"admin".equals(s)){
-            throw new UsernameNotFoundException("该用户无法找到!");
-        }else{
-            String role = "ROLE_ADMIN";
-            List<SimpleGrantedAuthority> authorities = new ArrayList<SimpleGrantedAuthority>();
-            authorities.add(new SimpleGrantedAuthority(role));
-            String password = passwordEncoder.encode("123456");
-            return new User(s,password,authorities);
-        }
-    }
-}
+//package com.ggz.service;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@Component(value = "customUserDetailsService")
+//public class CustomUserDetailsService implements UserDetailsService {
+//
+//    private final PasswordEncoder passwordEncoder;
+//
+//    public CustomUserDetailsService(PasswordEncoder passwordEncoder) {
+//        this.passwordEncoder = passwordEncoder;
+//    }
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+//        if(!"admin".equals(s)){
+//            throw new UsernameNotFoundException("该用户无法找到!");
+//        }else{
+//            String role = "ROLE_ADMIN";
+//            List<SimpleGrantedAuthority> authorities = new ArrayList<SimpleGrantedAuthority>();
+//            authorities.add(new SimpleGrantedAuthority(role));
+//            String password = passwordEncoder.encode("123456");
+//            return new User(s,password,authorities);
+//        }
+//    }
+//}
