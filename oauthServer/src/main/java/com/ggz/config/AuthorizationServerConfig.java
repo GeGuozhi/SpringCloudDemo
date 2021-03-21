3a28717d5e705053d95bd3c742ddfb0f61c08a0e
GeGuozhi <1462496578@qq.com>
2021-03-22 01:13:43 +0800

    不要纠结为什么错了，用对的就行了。

--- oauthServer/src/main/java/com/ggz/config/AuthorizationServerConfig.java
+++ oauthServer/src/main/java/com/ggz/config/AuthorizationServerConfig.java
@@ -1,87 +1,88 @@
-//package com.ggz.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.authentication.AuthenticationProvider;
-//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.core.AuthenticationException;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurer;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-//import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-//import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-//
-//
-//@Configuration
-//@EnableAuthorizationServer
-//public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
-//    public AuthorizationServerConfig() {
-//        super();
-//    }
-//
-//    @Override
-//    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-//        clients.inMemory().withClient("client_1").secret(passwordEncoder().encode("123456")).
-//                redirectUris("https://www.baidu.com/").authorizedGrantTypes("authorization_code").scopes("all").
-//                accessTokenValiditySeconds(7200);
-//    }
-//
-//    @Override
-//    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-//        //允许表单认证
-//        security.allowFormAuthenticationForClients();
-//        //允许check_token访问
+package com.ggz.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+    public AuthorizationServerConfig() {
+        super();
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory().withClient("client_1").secret(passwordEncoder().encode("123456")).
+                redirectUris("https://www.baidu.com/").authorizedGrantTypes("authorization_code").scopes("all").
+                accessTokenValiditySeconds(7200);
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        //允许表单认证
+        security.allowFormAuthenticationForClients();
+        //允许check_token访问
 //        security.checkTokenAccess("permitAll()");
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder(){
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//    @Bean
-//    UserDetailsService userDetailsService() {
-//        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
-//        userDetailsManager.createUser(User.withUsername("user_1").password(passwordEncoder().encode("123456")).
-//                authorities("ROLE_USER").build());
-//        userDetailsManager.createUser(User.withUsername("user_2").password(passwordEncoder().encode("1234567")).
-//                authorities("ROLE_USER").build());
-//        return userDetailsManager;
-//    }
-//
-//    @Override
-//    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-//        endpoints.authenticationManager(authenticationManager()).allowedTokenEndpointRequestMethods(HttpMethod.POST,HttpMethod.GET);
-//    }
-//
-//    @Bean
-//    public AuthenticationManager authenticationManager() {
-//        AuthenticationManager authenticationManager = new AuthenticationManager() {
-//            @Override
-//            public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-//                return daoAuthenticationProvider().authenticate(authentication);
-//            }
-//        };
-//        return authenticationManager;
-//    }
-//
-//    @Bean
-//    public AuthenticationProvider daoAuthenticationProvider(){
-//        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-//        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
-//        daoAuthenticationProvider.setUserDetailsService(userDetailsService());
-//        daoAuthenticationProvider.setHideUserNotFoundExceptions(false);
-//        return daoAuthenticationProvider;
-//    }
-//
-//
-//}
+        security.checkTokenAccess("isAuthenticated()");
+        security.tokenKeyAccess("isAuthenticated()");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+        userDetailsManager.createUser(User.withUsername("user_1").password(passwordEncoder().encode("123456")).
+                authorities("ROLE_USER").build());
+        userDetailsManager.createUser(User.withUsername("user_2").password(passwordEncoder().encode("1234567")).
+                authorities("ROLE_USER").build());
+        return userDetailsManager;
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager()).allowedTokenEndpointRequestMethods(HttpMethod.POST, HttpMethod.GET);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        AuthenticationManager authenticationManager = new AuthenticationManager() {
+            @Override
+            public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+                return daoAuthenticationProvider().authenticate(authentication);
+            }
+        };
+        return authenticationManager;
+    }
+
+    @Bean
+    public AuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService());
+        daoAuthenticationProvider.setHideUserNotFoundExceptions(false);
+        return daoAuthenticationProvider;
+    }
+
+
+}
--- oauthServer/src/main/java/com/ggz/config/ConfigBean.java
+++ oauthServer/src/main/java/com/ggz/config/ConfigBean.java
@@ -1,31 +1,31 @@
-package com.ggz.config;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
-import org.springframework.stereotype.Component;
-
-@Configuration
-@Component(value = "configBean")
-public class ConfigBean {
-    private final RedisConnectionFactory redisConnectionFactory;
-
-    public ConfigBean(RedisConnectionFactory redisConnectionFactory) {
-        this.redisConnectionFactory = redisConnectionFactory;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public TokenStore redisTokenStore(){
-        return new RedisTokenStore(redisConnectionFactory);
-    }
-}
+//package com.ggz.config;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.redis.connection.RedisConnectionFactory;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.oauth2.provider.token.TokenStore;
+//import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
+//import org.springframework.stereotype.Component;
+//
+//@Configuration
+//@Component(value = "configBean")
+//public class ConfigBean {
+//    private final RedisConnectionFactory redisConnectionFactory;
+//
+//    public ConfigBean(RedisConnectionFactory redisConnectionFactory) {
+//        this.redisConnectionFactory = redisConnectionFactory;
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder(){
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Bean
+//    public TokenStore redisTokenStore(){
+//        return new RedisTokenStore(redisConnectionFactory);
+//    }
+//}
