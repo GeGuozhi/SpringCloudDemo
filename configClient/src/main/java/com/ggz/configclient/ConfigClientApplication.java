19adf98e4da95f760b706d7f62b75e005e9a89ea
ggz <1462496578@qq.com>
2026-04-09 16:12:04 +0800

    Revert "测试回退"
    This reverts commit 60db78477c65cfc487cf63aee25b9c30a607e6de.
    
    # Conflicts:
    #	testFile.txt

--- configClient/src/main/java/com/ggz/configclient/ConfigClientApplication.java
+++ configClient/src/main/java/com/ggz/configclient/ConfigClientApplication.java
@@ -1,5 +1,6 @@
 package com.ggz.configclient;
 
+import cn.hutool.core.net.NetUtil;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
--- oauthServer/src/main/java/com/ggz/OauthServerApplication.java
+++ oauthServer/src/main/java/com/ggz/OauthServerApplication.java
@@ -1,5 +1,6 @@
 package com.ggz;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
