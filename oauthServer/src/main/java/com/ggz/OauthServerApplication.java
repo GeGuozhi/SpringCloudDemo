9f20176488d1ee63025e6e85324a979c3c41d25e
ggz <1462496578@qq.com>
2026-04-09 16:22:28 +0800

    master commit

--- oauthServer/src/main/java/com/ggz/OauthServerApplication.java
+++ oauthServer/src/main/java/com/ggz/OauthServerApplication.java
@@ -1,6 +1,5 @@
 package com.ggz;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
